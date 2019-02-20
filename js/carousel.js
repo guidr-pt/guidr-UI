44a7e4fd97730e5c7b8e65fb72c9002692a3fad6
cpscott1 <thacam18@gmail.com>
2019-02-19 22:00:30 -0600

    added functionality to carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,22 @@
-let slideInterval = 4000;
 let carousel = document.querySelector('.carousel');
 let figures = carousel.querySelectorAll('figure');
+let slideInterval = 4000;
+let pointer = 0;
+
+function moveForward() {
+  for(let i = 0; i < figures.length; i++) {
+    if(figures[i].className === 'visible') {
+      figures[i].className = 'hidden';
+      pointer = i;
+    }
+  }
+  if (++pointer === figures.length) {
+    pointer = 0;
+  }
+  figures[pointer].className = 'visible';
+  setTimeout(moveForward, slideInterval);
+}
+
+function startPlayback() {
+ setTimeout(moveForward, slideInterval);
+}
